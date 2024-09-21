e85d5ae0dd4131e0db39f31d66565ae6eda18429
chandrikakarre <150416611+chandrikakarre@users.noreply.github.com>
2024-09-21 12:47:25 +0530

    Update Header.js

--- Demo/src/components/Layout/Header.js
+++ Demo/src/components/Layout/Header.js
@@ -6,7 +6,7 @@ function Header() {
         <header>
             <h1>Placement Management System</h1>
             <nav>
-                <Link to="/">Home</Link> | <Link to="/students">Students</Link> | <Link to="/placements">Placements</Link>
+                  <Link to="/">Home</Link> | <Link to="/students">Students</Link> | <Link to="/placements">Placements</Link>|<Link to="/Login">Login</Link>|<Link to="/Signup">Signup</Link>
             </nav>
         </header>
     );
